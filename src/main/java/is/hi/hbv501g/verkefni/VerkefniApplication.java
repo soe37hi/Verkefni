12b246533b886bdf5f3cf509bbcb24d46eb73d2a
skyr567567 <135154965+skyr567567@users.noreply.github.com>
2023-09-10 03:19:13 +0000

    initial commit

---
+++ src/main/java/is/hi/hbv501g/verkefni/VerkefniApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.verkefni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerkefniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerkefniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/verkefni/VerkefniApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.verkefni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerkefniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
