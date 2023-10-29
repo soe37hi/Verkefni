36a480a9e98e33f84fcf0ed3d01d45e9f16dff7a
soe37hi <soe37hi@users.noreply.github.com>
2023-10-29 19:34:42 +0000

    moved controllers around + small changes

--- src/main/java/is/hi/hbv501g/verkefni/Controllers/GameController.java
+++ src/main/java/is/hi/hbv501g/verkefni/Controllers/GameController.java
@@ -2,6 +2,7 @@ package is.hi.hbv501g.verkefni.Controllers;
 
 import is.hi.hbv501g.verkefni.Persistence.Entities.Game;
 import is.hi.hbv501g.verkefni.Services.GameService;
+import is.hi.hbv501g.verkefni.Services.GameSessionService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +13,75 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 
 @Controller
 public class GameController {
 
     GameService gameService;
 
+    GameSessionService gameSessionService;
+
     @Autowired
-    public GameController(GameService gameService){
-
+    public GameController(GameSessionService gameSessionService, GameService gameService){
+        this.gameSessionService = gameSessionService;
         this.gameService = gameService;
+    }
 
+
+    @RequestMapping("/game")
+    public String gamePage(@RequestParam (name = "action", required = false, defaultValue = "no") String action, Model model, HttpSession session) {
+        Game previousMovie = (Game) session.getAttribute("game");
+
+        if (action.equals("yes") & previousMovie != null) {
+            gameService.increaseVote(previousMovie.getTitle(), previousMovie.getSessionID());
+        }
+
+        String sessionID = (String) session.getAttribute("sessionID");
+
+        Boolean check = (Boolean) session.getAttribute("firstMovie");
+        if (check) {
+            List<Game> games = gameService.findAllBySessionID(sessionID);
+            session.setAttribute("games", games);
+            session.setAttribute("firstMovie", Boolean.FALSE);
+        }
+
+        List<Game> games = (List<Game>) session.getAttribute("games");
+
+        if(games.isEmpty()) {
+            gameSessionService.updateStatus(sessionID);
+            return "redirect:/results";
+        }
+
+        Game game = games.get(0);
+        games.remove(0);
+        session.setAttribute("games", games);
+
+        model.addAttribute("game", game);
+        session.setAttribute("game", game);
+        return "game";
+    }
+
+
+
+    @RequestMapping(value = "/results", method = RequestMethod.GET)
+    public String results(HttpSession session, Model model){
+        String sessionID = (String) session.getAttribute("sessionID");
+
+        int playersJoined = gameSessionService.playersJoined(sessionID);
+        int playersFinishedVoting = gameSessionService.playersFinishedVoting(sessionID);
+
+        // Ef allir eru búnir að kjósa að þá förum við í results
+        if(playersJoined == (playersFinishedVoting-1)) {
+            List<Game> results = gameService.findAllBySessionID(sessionID);
+            model.addAttribute("results", results);
+
+            return "results";
+        }
+
+        // Annars höldum við okkur hér
+        return "preresults";
     }
 
 
--- src/main/java/is/hi/hbv501g/verkefni/Controllers/GameSessionController.java
+++ src/main/java/is/hi/hbv501g/verkefni/Controllers/GameSessionController.java
@@ -142,60 +142,6 @@ public class GameSessionController {
         return "redirect:/game";
     }
 
-    @RequestMapping("/game")
-    public String gamePage(@RequestParam (name = "action", required = false, defaultValue = "no") String action, Model model, HttpSession session) {
-        Game previousMovie = (Game) session.getAttribute("game");
-
-        if (action.equals("yes") & previousMovie != null) {
-            gameService.increaseVote(previousMovie.getTitle(), previousMovie.getSessionID());
-        }
-
-        String sessionID = (String) session.getAttribute("sessionID");
-
-        Boolean check = (Boolean) session.getAttribute("firstMovie");
-        if (check) {
-            List<Game> games = gameService.findAllBySessionID(sessionID);
-            session.setAttribute("games", games);
-            session.setAttribute("firstMovie", Boolean.FALSE);
-        }
-
-        List<Game> games = (List<Game>) session.getAttribute("games");
-
-        if(games.isEmpty()) {
-            gameSessionService.updateStatus(sessionID);
-            return "redirect:/results";
-        }
-
-        Game game = games.get(0);
-        games.remove(0);
-        session.setAttribute("games", games);
-
-        model.addAttribute("game", game);
-        session.setAttribute("game", game);
-        return "game";
-    }
-
-
-
-    @RequestMapping(value = "/results", method = RequestMethod.GET)
-    public String results(HttpSession session, Model model){
-        String sessionID = (String) session.getAttribute("sessionID");
-
-        int playersJoined = gameSessionService.playersJoined(sessionID);
-        int playersFinishedVoting = gameSessionService.playersFinishedVoting(sessionID);
-
-        // Ef allir eru búnir að kjósa að þá förum við í results
-        if(playersJoined == (playersFinishedVoting-1)) {
-            List<Game> results = gameService.findAllBySessionID(sessionID);
-            model.addAttribute("results", results);
-
-            return "results";
-        }
-
-        // Annars höldum við okkur hér
-        return "preresults";
-    }
-
 
     // Fall sem býr til random 6 stafa PIN fyrir game session
     // (1. tala er ekki 0)
